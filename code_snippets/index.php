2df262644f52a36a73663a8f6092e30cef7f534f
Dennis DeLarge <dennis_clausen@web.de>
2019-05-19 00:41:23 +0200

    Update index.php

--- code_snippets/index.php
+++ code_snippets/index.php
@@ -1,10 +1,3 @@
-<?php
-$destination = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-require_once('helper.php');
-
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
